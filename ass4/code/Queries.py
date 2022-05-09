4f4d6f245f3a3ce281d4826a49b7133fbda7cbb9
nicholas <nickptran@gmail.com>
2022-05-09 02:13:10 -0700

    completed query output

--- ass4/code/Queries.py
+++ ass4/code/Queries.py
@@ -7,18 +7,26 @@ cursor = conn.cursor()
 
 def query(filename):
     filepath = (f"input/queries/{filename}.sql")
+    outputdir = (f"output/queries/{filename}.txt")
+
     with open(filepath, "r")as sql:
         query = sql.read()
         results = cursor.execute(query).fetchall()
         colnames = cursor.description
+        appendtooutput = open(outputdir, "a")
 
         NameOfColumns = []
         for name in colnames:
             NameOfColumns.append(name[0])
         print(NameOfColumns)
+        appendtooutput.write(str(NameOfColumns)+'\n')
 
+        records = []
         for row in results:
-            print(row)
+            records.append(row)
+
+        for things in records:
+            appendtooutput.write(str(things)+'\n')
 
 
 query('customer')
